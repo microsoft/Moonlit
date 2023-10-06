9923d88c2f912b1423429b90248bf7076a950dbd
secretu <1449627099@qq.com>
2023-10-07 00:25:38 +0800

    add script

---
+++ LitePred/profile_script/profile.py
@@ -0,0 +1,9 @@
+from ADBConnect import *
+from bench_utils import*
+from run_on_device  import*
+
+
+device_seris = "1c19316"
+adb = ADBConnect(device_seris)
+model_path = '/path/to/your/model'
+_,avg,_ = run_on_android(model_path,adb)
